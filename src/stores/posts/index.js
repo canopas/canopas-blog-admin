9f0027ad415e7c6aad8c522873cbc0ba0e00f2fc
Shruti Sonani <shruti.s@canopas.com>
2022-11-10 13:00:04 +0530

    Fix the dynamic routing issue

--- src/stores/posts/index.js
+++ src/stores/posts/index.js
@@ -40,3 +40,46 @@ export const postStore = defineStore("posts", {
     },
   },
 });
+
+export const postDetailStore = defineStore("post-detail", {
+  state: () => {
+    return {
+      item: null,
+      error: null,
+      isLoading: false,
+    };
+  },
+  actions: {
+    async loadPostDetail(id) {
+      console.log("here");
+      // No need of API call if the item is already loaded
+      if (
+        this.item != null &&
+        this.item.unique_id == id &&
+        this.error == null
+      ) {
+        this.isLoading = false;
+        return;
+      }
+
+      return new Promise((resolve) => {
+        this.isLoading = true;
+        this.item = null;
+        this.error = null;
+
+        axios
+          .get(config.API_BASE + "/api/posts/" + id + "?populate=*")
+          .then((response) => {
+            this.item = response.data;
+            this.isLoading = false;
+            resolve();
+          })
+          .catch((error) => {
+            this.error = error;
+            this.isLoading = false;
+            resolve();
+          });
+      });
+    },
+  },
+});

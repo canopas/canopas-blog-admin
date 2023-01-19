2304a274dcbc40763cfda50bb65e555f8024b5dc
sumita <sumita.k@canopas.com>
2023-01-19 10:59:43 +0530

    Feat: add robots noindex,nofollow meta

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import "../assets/css/global.css";
+import Head from "next/head";
 import Header from "../components/partials/header";
 
 function BlogApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="robots" content="noindex, nofollow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Header></Header>
       <Component {...pageProps} />
     </>

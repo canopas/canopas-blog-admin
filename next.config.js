ef50fda762ce3f363869c3e6b190327f596d98d5
sumita <sumita.k@canopas.com>
2023-05-20 11:03:18 +0530

    Add basepath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  basePath: "",
+  basePath: "/resources",
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "assets")],

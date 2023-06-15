0a1a8790d3edfb3106c72113f270c490181f333b
mansi <mansi.d@canopas.com>
2023-06-15 11:05:44 +0530

    perf : remove robots meta from _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,6 @@ export default function App({ Component, pageProps }) {
     <div className="overflow-x-clip">
       <Head>
         <meta charset="UTF-8" />
-        <meta name="robots" content="noindex, nofollow" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/resources/favicon.ico" />
       </Head>

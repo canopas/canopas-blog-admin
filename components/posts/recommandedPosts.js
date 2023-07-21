f8542e2d01fa45c9c5d2f4a159573a5fe1c76558
mansi <mansi.d@canopas.com>
2023-07-21 19:45:25 +0530

    style : redesign recommanded section

--- components/posts/recommandedPosts.js
+++ components/posts/recommandedPosts.js
@@ -7,6 +7,7 @@ export default function RecommandedPosts({ postData }) {
   relatedPosts = relatedPosts.sort(
     (a, b) => b.attributes.index - a.attributes.index,
   );
+
   return (
     <>
       <span className="text-[1.7rem] lg:text-[2rem] xl:text-[1.6rem] font-semibold tracking-wide capitalize">
@@ -15,13 +16,20 @@ export default function RecommandedPosts({ postData }) {
       <div className="mt-8">
         {relatedPosts.map((post, i) => {
           post = post.attributes;
+          post.published_on = new Date(post.published_on).toLocaleDateString(
+            "en-US",
+            {
+              month: "short",
+              day: "numeric",
+            },
+          );
           if (i < 3) {
             return (
               <Link href={"/" + post.slug} key={i}>
-                <div className="grid grid-cols-3 gap-5 xl:gap-2 2xl:gap-5 mb-10 xl:mb-6 2xl:mb-10">
-                  <div className="flex flex-col col-span-2 space-y-1">
-                    <div className="flex flex-row space-x-2 items-center text-[0.8rem] md:text-[1rem] xl:text-[0.8rem] tracking-wider capitalize">
-                      <div className="relative w-[30px] md:w-[31px] h-[30px] max-w-full max-h-full overflow-hidden">
+                <div className="grid grid-cols-3 xl:grid-cols-2 gap-5 xl:gap-2 2xl:gap-5 mb-10 xl:mb-6 2xl:mb-7">
+                  <div className="flex flex-col col-span-2 space-y-2">
+                    <div className="flex flex-row space-x-2 items-center text-[0.8rem] md:text-[0.9rem] xl:text-[0.8rem] tracking-wider capitalize">
+                      <div className="relative w-[24px] h-[24px] md:w-[30px] md:h-[30px] xl:w-[24px] xl:h-[24px] max-w-full max-h-full overflow-hidden">
                         <Image
                           width={30}
                           height={30}
@@ -34,7 +42,7 @@ export default function RecommandedPosts({ postData }) {
                     </div>
 
                     <div
-                      className="text-[1rem] sm:text-[1.1rem] md:text-[1.4rem] xl:text-[1rem] font-bold leading-5 md:leading-7 xl:leading-5 tracking-wider xl:line-clamp-2"
+                      className="text-[1rem] sm:text-[1.1rem] md:text-[1.25rem] xl:text-[1rem] font-bold leading-5 md:leading-[1.7rem] xl:leading-5 tracking-wider xl:line-clamp-2"
                       onClick={() => {
                         mixpanel.track("tap_blog_title", {
                           Title: post.title,
@@ -43,14 +51,19 @@ export default function RecommandedPosts({ postData }) {
                     >
                       {post.title}
                     </div>
-                    <div className="text-gray-600 text-[1rem] md:text-[1.06rem] leading-6 md:leading-7 tracking-wide">
-                      <span className="xl:hidden line-clamp-1 lg:line-clamp-2">
+                    <div className="xl:hidden text-gray-600 text-[1rem] md:text-[1.06rem] leading-6 md:leading-7 tracking-wide">
+                      <span className="line-clamp-1 lg:line-clamp-2">
                         {post.summary}
                       </span>
                     </div>
+                    <div className="hidden xl:block text-gray-600 text-[0.9rem]">
+                      <span>{post.published_on}</span>
+                      <span className="after:content-['\00B7'] after:mx-1"></span>
+                      <span>{post.readingTime} min read</span>
+                    </div>
                   </div>
                   <div
-                    className={`mt-4 xl:mt-2 max-w-xs xl:h-[4rem] ${
+                    className={`xl:hidden mt-4 xl:mt-2 max-w-xs xl:h-[4rem] ${
                       post.image.data == null
                         ? "h-[5.084rem] md:h-[6.96rem] lg:h-[8.9rem] bg-black-900"
                         : "aspect-w-2 md:aspect-h-1 h-[5.084rem] border border-1"
